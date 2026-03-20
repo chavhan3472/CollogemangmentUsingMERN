5326858c5f4e16d8287751c7c1a7a77c83f4e8e2
chavhan3472 <sahilchavhan250@gmail.com>
2026-03-20 15:44:42 +0530

    Update port not

--- Backend/index.js
+++ Backend/index.js
@@ -17,4 +17,7 @@ let app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", mini_app);
-app.listen(5000);
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
