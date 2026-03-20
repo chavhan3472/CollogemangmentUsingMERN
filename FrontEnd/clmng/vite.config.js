d51bac607f16bcf199fcb0a3d9c9e6f6c4e7f94d
chavhan3472 <sahilchavhan250@gmail.com>
2026-03-20 19:40:45 +0530

    Fix allowedHosts for frontend-w1es.onrender.com

--- FrontEnd/clmng/vite.config.js
+++ FrontEnd/clmng/vite.config.js
@@ -17,17 +17,31 @@
 //     host: true, // external network access allow
 //   },
 // });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: process.env.PORT || 5173,
+//     host: true,
+//   },
+//   preview: {
+//     allowedHosts: ["collogemangmentusingmern-2.onrender.com"],
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT || 5173,
     host: true,
+    allowedHosts: ["frontend-w1es.onrender.com"], // live URL allow karo
   },
   preview: {
-    allowedHosts: ["collogemangmentusingmern-2.onrender.com"],
+    allowedHosts: ["frontend-w1es.onrender.com"], // preview ke liye bhi
   },
 });
