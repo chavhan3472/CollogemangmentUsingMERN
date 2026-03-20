249f188ce8febd323496c9c7cfb3cf3d1de35253
chavhan3472 <sahilchavhan250@gmail.com>
2026-03-21 00:41:40 +0530

    Add cookie parser and fix CORS

--- Backend/index.js
+++ Backend/index.js
@@ -2,22 +2,29 @@ let express = require("express");
 let mongoose = require("mongoose");
 let cors = require("cors");
 const mini_app = require("./router/router");
+const cookieParser = require("cookie-parser");
 mongoose
-  .connect(
-    "mongodb+srv://sahilchavhan250_db_user:PSTWrj2m0dQWQU28@cluster0.y9kpyzn.mongodb.net/CollageMangmentdatabse",
-  )
-  .then(() => {
-    console.log("Connection Ok");
-  })
-  .catch((error) => {
-    console.log(error);
-    console.log("Failed To Coonect ");
-  });
+    .connect(
+        "mongodb+srv://sahilchavhan250_db_user:PSTWrj2m0dQWQU28@cluster0.y9kpyzn.mongodb.net/CollageMangmentdatabse",
+    )
+    .then(() => {
+        console.log("Connection Ok");
+    })
+    .catch((error) => {
+        console.log(error);
+        console.log("Failed To Coonect ");
+    });
 let app = express();
-app.use(cors());
+app.use(
+    cors({
+        origin: ["https://frontend-w1es.onrender.com"],
+        credentials: true,
+    }),
+);
 app.use(express.json());
+app.use(cookieParser());
 app.use("/", mini_app);
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
