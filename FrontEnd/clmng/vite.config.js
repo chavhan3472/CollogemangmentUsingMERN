1a7272be8fa7eb1f70e07a023fd1147e9ec4c5ad
chavhan3472 <sahilchavhan250@gmail.com>
2026-03-20 18:33:31 +0530

    Render deploy fix: dynamic port and preview script

--- FrontEnd/clmng/vite.config.js
+++ FrontEnd/clmng/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // Render ka dynamic port
+    host: true, // external network access allow
+  },
+});
