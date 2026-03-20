3b9c9645ebe5f40b361cd5ee1b81bb9cc8daf7b3
chavhan3472 <sahilchavhan250@gmail.com>
2026-03-20 19:24:57 +0530

    Fix blocked request: add allowedHosts

--- FrontEnd/clmng/vite.config.js
+++ FrontEnd/clmng/vite.config.js
@@ -6,6 +6,17 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: process.env.PORT || 5173, // Render ka dynamic port
+//     host: true, // external network access allow
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -13,7 +24,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173, // Render ka dynamic port
-    host: true, // external network access allow
+    port: process.env.PORT || 5173,
+    host: true,
+  },
+  preview: {
+    allowedHosts: ["collogemangmentusingmern-2.onrender.com"],
   },
 });
